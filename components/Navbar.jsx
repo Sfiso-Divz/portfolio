0d561969569fd7e58842fe05a078f59e2143040a
Mfihlo Dlamini <115468243+Sfiso-Divz@users.noreply.github.com>
2025-02-03 12:34:02 +0200

    completed

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -83,7 +83,7 @@ const Navbar = () => {
                         <p className='uppercase tracking-widest text-[#5651e5] '>Let's connect</p>
                         <div className='flex justify-between items-center my-4 w-full sm:w-[80%] '>
                             <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-500'>
-                                <a href="https://www.linkedin.com/in/mfihlo-dlamini-7193092a3/"><FaLinkedin /></a>
+                                <a href="https://www.linkedin.com/in/mfihlo-dlamini-7193092a3/"><FaLinkedinIn /></a>
                             </div>
                             <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-500'>
                                  <a href="https://github.com/Sfiso-Divz"><FaGithub /></a>
